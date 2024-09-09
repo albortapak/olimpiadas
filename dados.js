78e37445d62c6961c064f85ba6fc8e6f4940b4a0
Alan Kristian Alborta Perez <48145166+albortapak@users.noreply.github.com>
2024-09-09 17:46:28 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+  {
+    titulo: "Rayssa Leal",
+    descricao: "Jhulia Rayssa Mendes Leal (Imperatriz, 4 de janeiro de 2008) é uma skatista brasileira, vice-campeã olímpica nos Jogos Olímpicos de Verão de 2020 em Tóquio, sendo a mais jovem medalhista olímpica brasileira.",
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags: "skate fada sk8"
+  },
+  {
+    titulo: "Beatriz Souza",
+    descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nos Jogos Olímpicos de Paris 2024.",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+    tags: "judô judo judoca judoka"
+  },
+  {
+    titulo: "Rebeca Andrade",
+    descricao: "Rebeca Andrade é uma ginasta brasileira, campeã olímpica nos Jogos Olímpicos de Paris 2024.",
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    tags: "ginástica ginastica ouro"
+  }
+];
